d54472e93f952e695f4dec4dfc1d60d1dcc7da79
AnchuK <anchukariyapath@gmail.com>
2021-11-14 00:11:59 +0530

    change

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function validation()
     let regexp = /^([A-Za-z0-9\.-]+)@([A-Za-z0-9\-]+).([a-z]{2,3})(.[a-z]{2,3}?)$/
     if(regexp.test(email.value))
     {
-        error1.innerHTML = "Valid";
+        error1.innerHTML = "";
         error1.style.color="green";
         email.style.borderColor =  "#27ae60";
         return true;
@@ -48,7 +48,7 @@ function passwordChanged()
     let regpass = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/
     if(regpass.test(password.value))
     {
-        error2.innerHTML = "Valid";
+        error2.innerHTML = "";
         error2.style.color="green";
         password.style.borderColor =  "#27ae60";
         return true;
@@ -75,7 +75,7 @@ function passwordChanged5()
     let regpass = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/
     if(regpass.test(password5.value))
     {
-        error5.innerHTML = "Valid";
+        error5.innerHTML = "";
         error5.style.color="green";
         password.style.borderColor =  "#27ae60";
         return true;
@@ -105,7 +105,7 @@ function validationph()
     let regph = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
     if(regph.test(ph.value))
     {
-        error3.innerHTML = "Valid";
+        error3.innerHTML = "";
         error3.style.color="green";
         ph.style.borderColor =  "#27ae60";
         return true;
